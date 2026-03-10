bac01878635846b6332fc989cee89d230b842061
MinePopTheReal <realminepop@gmail.com>
2026-03-10 19:13:46 +0100

    I finish parsing

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: tmalpert <tmalpert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 18:44:59 by tmalpert          #+#    #+#             */
-/*   Updated: 2026/03/10 14:16:50 by tmalpert         ###   ########.fr       */
+/*   Updated: 2026/03/10 17:35:08 by tmalpert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,33 +21,41 @@
 
 #include "codexion.h"
 
-static int is_valid_number(char *parse_value)
+static void set_value(char **argv, t_parsing_val *parse_value)
 {
-	int value;
-
+	parse_value->number_of_coder = atoi(argv[1]);
+	parse_value->time_to_burnout = atoi(argv[2]);
+	parse_value->time_to_compile = atoi(argv[3]);
+	parse_value->time_to_debug = atoi(argv[4]);
+	parse_value->time_to_refactor = atoi(argv[5]);
+	parse_value->number_of_compiles = atoi(argv[6]);
+	parse_value->dongle_cooldown = atoi(argv[7]);
+	parse_value->scheduler = argv[8];
+}
+static bool is_valid_number(char *parse_value)
+{
 	if (!parse_value)
-		return (-1);
-	value = atoi(parse_value);
-	if (value <= 0)
-		return (-1);
-	return (value);
+		return (false);
+	if (parse_value[0] == '-')
+		return (false);
+	return (true);
 }
 
-t_parsing_val parsing(int argc, char **argv)
+bool parsing(int argc, char **argv, t_parsing_val *parse_value)
 {
-	t_parsing_val parse_value;
+	int i;
 
-	memset(&parse_value, 0, sizeof(parse_value));
+	i = 0;
 	if (argc != 9)
-		return (parse_value);
-	parse_value.number_of_coder = is_valid_number(argv[1]);
-	parse_value.time_to_burnout = is_valid_number(argv[2]);
-	parse_value.time_to_compile = is_valid_number(argv[3]);
-	parse_value.time_to_debug = is_valid_number(argv[4]);
-	parse_value.time_to_refactor = is_valid_number(argv[5]);
-	parse_value.number_of_compiles = is_valid_number(argv[6]);
-	parse_value.dongle_cooldown = is_valid_number(argv[7]);
-	if (strcmp(argv[8], "fifo") || strcmp(argv[8], "edf"))
-		parse_value.scheduler = argv[8];
-	return (parse_value);
+		return (false);
+	while (i < 8)
+	{
+		if (!is_valid_number(argv[i]))
+			return (false);
+		i++;
+	}
+	if (!(strcmp(argv[i], "fifo") == 0 || strcmp(argv[i], "edf") == 0))
+		return (false);
+	set_value(argv, parse_value);
+	return (true);
 }
